33070bbf61895e35c32d2ed77eff3b352b8b1a94
Bia <biaalperes@gmail.com>
2023-11-23 16:06:36 -0300

    merge-tables

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,49 @@
+CREATE TABLE Livros(
+    id INT NOT NULL,
+    cod INT NOT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    autor VARCHAR(150),
+    preco VARCHAR(10),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Secoes(
+    id INT NOT NULL,
+    cod INT NOT NULL,
+    nome VARCHAR(150),
+    localizacao VARCHAR(150),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Vendas(
+    id INT NOT NULL,
+    cod INT NOT NULL,
+    data_venda VARCHAR(150),
+    numero VARCHAR(150),
+    id_cliente INT,
+    id_vendedor INT,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
+    FOREIGN KEY (id_vendedor) REFERENCES Vendedores(id) PRIMARY KEY(id)
+);
+
+CREATE TABLE Itens_Vendidos(
+    id INT NOT NULL,
+    cod INT NOT NULL,
+    sequencia VARCHAR(150),
+    livro VARCHAR(150),
+    quantidade INT,
+    preco_unitario INT,
+    preco_total INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Vendedores(
+    id INT NOT NULL,
+    cod INT NOT NULL,
+    nome VARCHAR(150),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE Clientes(
      id INT NOT NULL,
      cod INT NOT NULL,
